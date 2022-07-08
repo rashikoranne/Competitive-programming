f31043746970ffbfdd14ce003e481f5846ca43e1
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-08 23:09:23 +0530

    Update CHEFCAND.java

--- CHEFCAND.java
+++ CHEFCAND.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import java.util.Scanner;
+/*There are N children and Chef wants to give them 1 candy each. Chef already has X candies with him. To buy the rest, he visits a candy shop. In the shop, packets containing exactly 4 candies are available.
+
+Determine the minimum number of candy packets Chef must buy so that he is able to give 1 candy to each of the N children.*/
 
 public class CHEFCAND {
     public static void main(String[] args) {
