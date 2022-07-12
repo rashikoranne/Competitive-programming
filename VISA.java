0cedaab09e7b371dd58d9db8d3fec83e39ff20af
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-12 14:44:05 +0530

    Update VISA.java

--- VISA.java
+++ VISA.java
@@ -1,6 +1,14 @@
 package com.example;
 
 import java.util.Scanner;
+/*Ash is trying to get visa to Chefland. For the visa to be approved, he needs to satisfy the following three criteria:
+
+Solve at least x1 problems on Codechef.
+Have at least y1 current rating on Codechef.
+Make his last submission at most z1 months ago.
+You are given the number of problems solved by Chef (x2) 
+his current rating (y2) and the information that he made his last submission (z2) months ago. 
+Determine whether he will get the visa.*/
 
 public class VISA {
     public static void main(String[] args) throws java.lang.Exception {
