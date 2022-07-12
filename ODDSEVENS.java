15844c3e5859223dddfdad291f6a4218adf76bd1
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-13 05:20:42 +0530

    Update ODDSEVENS.java

--- ODDSEVENS.java
+++ ODDSEVENS.java
@@ -1,6 +1,12 @@
 package com.example;
 
 import java.util.Scanner;
+/*Alice and Bob play the classic game of odds and evens. In this game, each of the two players choose a number between 1 and 5 without revealing to their opponent. 
+Both of the players then simultaneously reveal their number by holding up that many fingers of their hand.
+Then the winner is decided based upon whether the sum of numbers played by both the players is odd or even. 
+In this game we assume that if the sum is odd then Alice wins, otherwise Bob wins.
+
+If Alice held up aa fingers and Bob held up bb fingers, determine who won the game.*/
 
 public class ODDSEVENS {
     public static void main (String[] args) throws java.lang.Exception
