eb308ae0a059428c9dfd4e9b300af81b4446d1a6
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-13 05:13:04 +0530

    Add files via upload

---
+++ Relative.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Relative {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int g = sc.nextInt();
+            int c = sc.nextInt();
+            int result  = (c * c)/(2*g);
+            System.out.println(result);
+        }
+
+    }
+}
