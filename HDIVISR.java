ac60e735bef5013f0ab4935c433cde3fb2c3a7df
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-12 23:38:03 +0530

    Add files via upload

---
+++ HDIVISR.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class HDIVISR {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 0;
+        int[] ar = new int[]{1,2,3,4,5,6,7,8,9,10};
+        for(int i = ar.length-1; i>=0; i--){
+            if(n%ar[i]==0){
+                num = ar[i];
+                break;
+
+            }
+
+        }
+        System.out.println(num);
+
+
+    }
+}
