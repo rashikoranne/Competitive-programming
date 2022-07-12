4279ffb3770c3bd63949b77fb8d2ade8130f3d44
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-12 15:30:59 +0530

    Update DEVSPORTS.java

--- DEVSPORTS.java
+++ DEVSPORTS.java
@@ -1,6 +1,11 @@
 package com.example;
 
 import java.util.Scanner;
+/*Recently, Devendra went to Goa with his friends. Devendra is well known for not following a budget.
+
+He had Rs. ZZat the start of the trip and has already spent Rs. Y on the trip. There are three kinds of water sports one can enjoy, with prices Rs. A, B, and C. He wants to try each sport at least once.
+
+If he can try all of them at least once output YES, otherwise output NO.*/
 
 public class DEVSPORTS {
     public static void main(String[] args) throws java.lang.Exception {
