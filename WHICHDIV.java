e862170726ca703a862f75d2215b71fadbdf6dd6
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-13 04:45:35 +0530

    Update WHICHDIV.java

--- WHICHDIV.java
+++ WHICHDIV.java
@@ -1,6 +1,14 @@
 package com.example;
 
 import java.util.Scanner;
+/*Given the rating RR of a person, tell which division he belongs to. The rating range for each division are given below:
+
+Division 1: 2000≤ Rating.
+
+Division 2: 1600≤ Rating <2000.
+
+Division 3: Rating<1600. 
+For each test case, output on a single line the answer: 1 if the person belongs to Division 1, 2 if the person belongs to Division 2, and 3 if the person belongs to Division 3.*/
 
 public class WHICHDIV {
     public static void main(String[] args) throws java.lang.Exception {
