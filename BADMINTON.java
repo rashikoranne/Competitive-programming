807a3bd73484e35f66fea78283ae17f25a6db65b
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-13 05:06:25 +0530

    Update BADMINTON.java

--- BADMINTON.java
+++ BADMINTON.java
@@ -1,6 +1,14 @@
 package com.example;
 
 import java.util.Scanner;
+/*Chef is playing badminton today. The service rules of this singles game of badminton are as follows:
+
+The player who starts the game serves from the right side of their court.
+Whenever a player wins a point, they serve next.
+If the server has won an even number of points during a game, then they will serve from the right side of the service court for the subsequent point.
+Chef will be the player who begins the game.
+
+Given the number of points PP obtained by Chef at the end of the game, please determine how many times Chef served from the right side of the court.*/
 
 public class BADMINTON {
     public static void main (String[] args) throws java.lang.Exception
