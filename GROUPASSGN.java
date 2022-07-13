e04feefd9f74995b0aa23493ccdc8133ead532d5
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-14 04:03:07 +0530

    Add files via upload

---
+++ GROUPASSGN.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class GROUPASSGN {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int totalstu = 2 * n;
+            int roll = Math.abs(x-totalstu);
+            System.out.println(roll+1);
+        }
+
+
+    }
+
+}
