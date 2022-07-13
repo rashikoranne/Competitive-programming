1be535b458821b4c926b373babd70e3ded8d9eea
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-07-14 04:03:32 +0530

    Update GROUPASSGN.java

--- GROUPASSGN.java
+++ GROUPASSGN.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Scanner;
+/*https://www.codechef.com/submit/GROUPASSGN*/
 
 public class GROUPASSGN {
     public static void main (String[] args) throws java.lang.Exception
