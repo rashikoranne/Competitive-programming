b72f063c7ab7422cb492a357620866e1b4fdae7d
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-10-06 08:53:30 +0530

    Add files via upload

---
+++ HAPPYSTR.java
@@ -0,0 +1,39 @@
+package com.example;
+import java.util.Scanner;
+
+public class HAPPYSTR {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+    Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    String s = sc.next();
+            int count = 0;
+            int flag = -1;
+            for(int i = 0; i<s.length(); i++){
+                if(s.charAt(i)== 'a'|| s.charAt(i) == 'e'|| s.charAt(i)=='i'|| s.charAt(i)=='o'||s.charAt(i)=='u'){
+                    count++;
+                    if(count > 2){
+                        flag = 0;
+                        break;
+                    }
+
+                }
+                else{
+                    count = 0;
+                    flag = 1;
+                }
+
+
+            }
+            if(flag == 0){
+                System.out.println("Happy");
+            }
+            else{
+                System.out.println("Sad");
+            }
+            
+    
+}
+    }
+}
