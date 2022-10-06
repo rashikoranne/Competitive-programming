a8bc58a011c1007b92bc7aac47452c4923b3af73
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-10-06 08:51:36 +0530

    Add files via upload

---
+++ susstr.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        char s[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>s[i];
+        }
+
+        int le=0,ri=n-1;
+
+        list<char> lf;
+
+        for (int i = 0; i < n; i++)
+        {
+            if(i%2==0){
+                if(s[le]=='0'){
+                    lf.push_front(s[le]);
+
+                }else if (s[le]=='1'){
+                    lf.push_back(s[le]);
+                }
+                le++;
+            }else{
+                if(s[ri]=='0'){
+                    lf.push_back(s[ri]);
+                }else if(s[ri]=='1'){
+                    lf.push_front(s[ri]);
+                }
+                ri--;
+            }
+            }
+        for (auto it = lf.begin(); it != lf.end(); it++)
+         cout << *it;
+
+    cout<<endl;
+    }
+
+    return 0;
+}
