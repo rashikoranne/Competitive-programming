1cd1b7e56d2a6b799b5705a7e0a2761cff3ff15c
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-10-06 08:58:17 +0530

    Add files via upload

---
+++ speciality.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class speciality {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int z = sc.nextInt();
+		    if(x>y && x>z){
+		        System.out.println("Setter");
+		    }
+		    else if(y>x && y>z){
+		        System.out.println("Tester");
+		    }
+		    else{
+		        System.out.println("Editorialist");
+		    }
+		}
+	}
+    
+}
