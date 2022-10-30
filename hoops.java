96d95091478edf954e6d3701d14eec19c86148fc
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-10-30 22:26:43 +0530

    Create hoops.java

---
+++ hoops.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		if(n==1){
+		    System.out.println(1);
+		}
+		else{
+		    System.out.println((n/2)+1);
+		}
+		}
+	}
+}
