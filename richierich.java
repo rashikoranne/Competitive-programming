1f6abe474b562ac200c7b01f15744c32fc9bb475
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-10-30 21:53:51 +0530

    Add files via upload

---
+++ richierich.java
@@ -0,0 +1,17 @@
+package com.example;
+import java.util.Scanner;
+
+public class richierich {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int x = sc.nextInt();
+		System.out.println((b-a)/x);
+	}
+        
+    }
+    
+}
