17be31f890abea2079796a8dba5a25548cc312ce
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-11-01 13:14:01 +0530

    Create twodish.java

---
+++ twodish.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    int count = 0;
+		    if(b>=n && (a+c)>=n){
+		        System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		   
+		    
+		}
+	}
+}
