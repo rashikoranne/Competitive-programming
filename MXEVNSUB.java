d9effcaa80db7ebf93fa854e2d962d2a77f4481f
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-11-01 13:00:01 +0530

    Update MXEVNSUB.java

--- MXEVNSUB.java
+++ MXEVNSUB.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+//https://www.codechef.com/LP1TO201/problems/MXEVNSUB
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
