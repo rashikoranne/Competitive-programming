ba0e7f0865d2925c434afbb8016fd78dbc461f90
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-11-01 12:59:26 +0530

    Create MXEVNSUB.java

---
+++ MXEVNSUB.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int sum = 0;
+		    int count = 0;
+		    while(n>0){
+		        sum = sum + n;
+		        n--;
+		        count++;
+		        
+		    }
+		    if(sum%2==0){
+		        System.out.println(count);
+
+		    }
+		    else{
+		        System.out.println(count-1);
+		    }
+		    
+		}
+		    
+	}
+}
