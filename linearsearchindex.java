3f5d60463afe9ef0e5ac00f93abece53754011d2
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-11-14 23:38:04 +0530

    Add files via upload

---
+++ linearsearchindex.java
@@ -0,0 +1,32 @@
+package com.example;
+
+public class linearsearchindex {
+    public static void main(String[] args) {
+        int[] arr = {1, 2 , 34, 55, 44, 56, 92};
+        int n = 55;
+        search(arr, n);
+        if(search(arr, n) == -1){
+            System.out.println("Element not found");
+        }
+        else{
+            System.out.println("The element was fount at the index: " + search(arr, n));
+        }
+        
+
+    }
+    static int search(int[] arr, int a){ 
+        int index = -1;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == a){
+                index = i;
+             }
+       
+        }
+        return index;
+       
+        
+        
+    }
+}
+    
+
