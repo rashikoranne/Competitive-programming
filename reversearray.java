95bd488f4793cf54cc090893bc233e279e08fa1d
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2022-11-14 22:07:26 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class reversearray {public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5};
+    reverse(arr);
+    System.out.println(Arrays.toString(arr));
+
+}
+static void reverse(int[] arr){
+    int start = 0;
+    int end = arr.length-1;
+    while(start<end){
+    swap(arr, start, end);
+    start++;
+    end--;
+    }
+    
+    
+}
+static void swap(int[] arr, int a, int b){
+    int temp = arr[a];
+    arr[a] = arr[b]; 
+    arr[b] = temp;
+    
+}
+    
+}
